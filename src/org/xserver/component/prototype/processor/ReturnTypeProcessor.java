41c4c24e0893d30f2d5e03d2eb4c345c22f6438f
poston <poston1@163.com>
2016-06-08 16:22:19 +0800

    Create ReturnTypeProcessor.java

---
+++ src/org/xserver/component/prototype/processor/ReturnTypeProcessor.java
@@ -0,0 +1,10 @@
+package org.xserver.component.prototype.processor;
+
+/**
+ * the intefacr of the ReturnTypeProcessor
+ * @author Idol
+ * @since  2016/02/23
+ */
+public interface ReturnTypeProcessor {
+	public Object returnProcess(Object obj);
+}
