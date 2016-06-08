75fd4341b64c853aec1ba9f1ba1f87a6c59c0d9c
poston <poston1@163.com>
2016-06-08 16:24:47 +0800

    Create XmlReturnTypeProcessor.java

---
+++ src/org/xserver/component/prototype/processor/impl/XmlReturnTypeProcessor.java
@@ -0,0 +1,21 @@
+package org.xserver.component.prototype.processor.impl;
+
+import org.springframework.stereotype.Component;
+import org.xserver.component.prototype.processor.ReturnTypeProcessor;
+
+/**
+ *  Just do the result with XmlProcessor
+ * @author Idol
+ * @since  2016/02/22
+ */
+@Component
+public class XmlReturnTypeProcessor implements ReturnTypeProcessor {
+
+	@Override
+	public Object returnProcess(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
