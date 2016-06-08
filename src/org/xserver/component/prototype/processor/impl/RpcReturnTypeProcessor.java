0c38f3c269c71334aca9043517f89989d5b441c7
poston <poston1@163.com>
2016-06-08 16:23:18 +0800

    Create RpcReturnTypeProcessor.java

---
+++ src/org/xserver/component/prototype/processor/impl/RpcReturnTypeProcessor.java
@@ -0,0 +1,13 @@
+package org.xserver.component.prototype.processor.impl;
+
+import org.xserver.component.prototype.processor.ReturnTypeProcessor;
+
+public class RpcReturnTypeProcessor implements ReturnTypeProcessor {
+
+	@Override
+	public Object returnProcess(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
