f02f627f1837f80649b8f759d2926da57c36853b
poston <poston1@163.com>
2016-06-08 15:38:59 +0800

    Create HttpRequestCookieAware.java

---
+++ src/org/xserver/component/core/http/HttpRequestCookieAware.java
@@ -0,0 +1,7 @@
+package org.xserver.component.core.http;
+
+import org.xserver.component.core.http.cookie.CookieAware;
+
+public interface HttpRequestCookieAware extends HttpRequestAware, CookieAware {
+
+}
